aa936602968783943a9d0ffa412359e1aced629f
mattsonthieme <mattson.thieme@gmail.com>
2020-02-27 14:49:00 -0600

    Cleaning up; TODOs

--- gnn.py
+++ gnn.py
@@ -1,3 +1,11 @@
+# TODO
+
+# Randomize data_list
+# Check if we're predicting the right velocity and if we did indeed move up or down in the final states
+# Organize/cleanup
+# Save models
+
+
 import torch
 from torch_geometric.data import Data, DataLoader
 import csv
@@ -5,6 +13,14 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import add_self_loops, degree
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from tqdm import tqdm
+import random
 
 data_list = []
 bulk_data = []
@@ -23,8 +39,10 @@ edge_index = torch.tensor([[0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3],
 period = 3  # 3*5s = 15s period
 max_index = len(bulk_data)-len(bulk_data)%period  # Don't overshoot
 data = np.array(bulk_data[:max_index]).astype(float)
-batch_size = 32
+batch_size = 128
 look_ahead = 60  # Steps to look ahead, will become the label
+num_epochs = 5
+train_test_split = 0.8
 
 # Fill this with data loaders - still not sure how to save this
 loaders = []
@@ -61,14 +79,6 @@ for row in range(len(data) - look_ahead):
     x, y = pos_vel_next(data[row - 1], data[row], data[row + look_ahead - 1], data[row + look_ahead])
     data_list.append(Data(x=x, y=y, edge_index=edge_index))
 
-
-from torch_geometric.nn import MessagePassing
-from torch_geometric.utils import add_self_loops, degree
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
-
 # Simple 2 layer GCN
 class Net(torch.nn.Module):
     def __init__(self):
@@ -81,96 +91,52 @@ class Net(torch.nn.Module):
 
         x = self.conv1(x, edge_index)
         x = F.relu(x)
-        x = F.dropout(x, 0.0)#training=self.training)
+        x = F.dropout(x, 0.0)
         x = self.conv2(x, edge_index)
 
         return x
 
-
-from tqdm import tqdm
-import random
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net().to(device)
-
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-train_test_split = 0.8
-
-
-#data = data[0:int(len(data)/10)]
 train_data = data_list[0:int(len(data_list)*train_test_split)]
 test_data = data_list[int(len(data_list)*train_test_split):]
-
 
 train_data = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 test_data = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
 
-
 model.train()
 
-for epoch in range(2):
+for epoch in range(num_epochs):
     loss_track = 0
     for batch in tqdm(train_data):
 
         optimizer.zero_grad()
         out = model(batch)
-
         loss = F.mse_loss(out, batch.y)
         loss_track += loss.item()
-        #print("Out =    ", out.squeeze(1))
-        #print("Data.y = ", data.y.squeeze(1))
-        #print("Loss = ", loss)
         loss.backward()
         optimizer.step()
 
-    print("Avg loss: ", loss_track/len(train_data))
+    print("Epoch {}/{}, Avg loss: {}".format(epoch, num_epochs, loss_track/len(train_data)))
 
 model.eval()
 
-loss = 0
+loss_track = 0
 
 final_out = None
 final_batch = None
 
 for batch in tqdm(test_data):
         out = model(batch)
-        loss += F.mse_loss(out, batch.y)
+        loss_track += F.mse_loss(out, batch.y)
         final_out = out
         final_batch = batch.y
 
-print("Final loss: ", loss/len(train_data))
+print("Final loss: ", loss_track/len(train_data))
 
-
-
-print(final_out)
-print(final_batch)
-
-'''
-    for example in tqdm(train_data):
-        #print("Data: ", data)
-
-
-        optimizer.zero_grad()
-        out = model(example)
-
-        loss = F.mse_loss(out, example.y)
-        #print("Out =    ", out.squeeze(1))
-        #print("Data.y = ", data.y.squeeze(1))
-        #print("Loss = ", loss)
-        loss.backward()
-        optimizer.step()
-
-'''
-
-
-
-
-
-
-
-
-
-
-
+#print(final_out)
+#print(final_batch)
 
